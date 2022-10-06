52915b93f73cd964084add1259bf8d76ede094f7
Jansen Fuller <jansendfuller@mailfence.com>
2022-10-05 21:11:51 -0600

    Initial commit

---
+++ Twitter Menu Bar/Twitter_Menu_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_Menu_BarApp.swift
+//  Twitter Menu Bar
+//
+//  Created by Jansen Fuller on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_Menu_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
